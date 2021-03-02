9a5f4650b95b4d0254b86b56226683cfcf98865c
Dain Vugs <dain.vugs@rocva.nl>
2021-03-02 13:44:24 +0100

    opdracht 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,12 @@
-
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(5, 'Jan', 'Klaas', '9A', 'bla 1', '2837BY', '2000-11-11', 'Amsterdam');
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(6, 'Gert', 'Jan', '9A', 'bla 2', '2834BY', '2000-10-10', 'Amsterdam');
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(7, 'Dan', 'Gert', '9A', 'bla 3', '2833BY', '2000-9-9', 'Amsterdam');
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(8, 'Henk', 'Dan', '9A', 'bla 4', '2832BY', '2000-8-8', 'Amsterdam');
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(9, 'Don', 'Henk', '9A', 'bla 5', '2831BY', '2000-7-7', 'Amsterdam');
+INSERT INTO studenten(student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+VALUES(10, 'Klaas', 'Don', '9A', 'bla 6', '2830BY', '2000-6-6', 'Amsterdam');
